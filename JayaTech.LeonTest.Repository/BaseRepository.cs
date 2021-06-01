a025ced0a200e9eeaf1d6d13933fec1b1230a79d
leonfgama <leonfgama@gmail.com>
2021-05-31 21:28:50 -0300

    Create WebAPI Authentication, Entities and Generic Interfaces

---
+++ JayaTech.LeonTest.Repository/BaseRepository.cs
@@ -0,0 +1,45 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using JayaTech.LeonTest.Domain.Entities;
+using JayaTech.LeonTest.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace JayaTech.LeonTest.Repository
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        public IDynamoDBContext DbContext { get; set; }
+
+        public void Delete(TEntity obj)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Insert(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JayaTech.LeonTest.WebAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,33 @@
+﻿using JayaTech.LeonTest.Domain.Entities;
+using JayaTech.LeonTest.WebAPI.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JayaTech.LeonTest.WebAPI.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            if (user == null)
+                return NotFound(new { message = "User or password is incorrect!" });
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
