15612792c6ebe2b1258cf790b893c7a25994d56d
leonfgama <leonfgama@gmail.com>
2021-05-31 21:31:04 -0300

    Fix bugs

--- JayaTech.LeonTest.Repository/BaseRepository.cs
+++ JayaTech.LeonTest.Repository/BaseRepository.cs
@@ -1,5 +1,4 @@
-﻿using Amazon.DynamoDBv2.DataModel;
-using JayaTech.LeonTest.Domain.Entities;
+﻿using JayaTech.LeonTest.Domain.Entities;
 using JayaTech.LeonTest.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -9,8 +8,6 @@ namespace JayaTech.LeonTest.Repository
     public class BaseRepository<TEntity> : IBaseRepository<TEntity>
         where TEntity : BaseEntity
     {
-        public IDynamoDBContext DbContext { get; set; }
-
         public void Delete(TEntity obj)
         {
             
--- JayaTech.LeonTest.WebAPI/Controllers/AuthenticationController.cs
+++ JayaTech.LeonTest.WebAPI/Controllers/AuthenticationController.cs
@@ -16,18 +16,20 @@ namespace JayaTech.LeonTest.WebAPI.Controllers
         [AllowAnonymous]
         public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
         {
-            var user = UserRepository.Get(model.Username, model.Password);
+            //var user = UserRepository.Get(model.Username, model.Password);
 
-            if (user == null)
-                return NotFound(new { message = "User or password is incorrect!" });
+            //if (user == null)
+            //    return NotFound(new { message = "User or password is incorrect!" });
 
-            var token = TokenService.GenerateToken(user);
-            user.Password = "";
-            return new
-            {
-                user = user,
-                token = token
-            };
+            //var token = TokenService.GenerateToken(user);
+            //user.Password = "";
+            //return new
+            //{
+            //    user = user,
+            //    token = token
+            //};
+
+            return new { test = "test" };
         }
     }
 }
